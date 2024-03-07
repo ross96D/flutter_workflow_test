44e55b04dbd32ca1a2ffcad24637e0762f687b6e
ross96D <abelaross96@gmail.com>
2024-03-07 12:41:26 -0500

    fix some shits

--- update_pubspec.py
+++ update_pubspec.py
@@ -1,11 +1,17 @@
-def main():
+import sys
+
+
+def main(token: str):
     new_content: list[str] = []
     filename = "pubspec.yaml"
     with open(filename) as file:
         content = file.readlines()
         for line in content:
             if "url: https://github.com/" in line:
-                line = line.replace("url: https://github.com/", "url: git@github.com:")
+                line = line.replace(
+                    "url: https://github.com/",
+                    f"url: ross96D:{token}@github.com/",
+                )
             new_content.append(line)
 
     with open(filename, "wt") as file:
@@ -13,4 +19,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    if sys.argv.__len__() != 1:
+        print("update pubspec takes 1 argument")
+        exit(1)
+    main(sys.argv[1])
